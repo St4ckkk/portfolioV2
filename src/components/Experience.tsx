b8d0d58322996c28f7c367344976fb93e5c49089
St4ckkk <keyanandydelgado@gmail.com>
2025-09-02 14:54:54 +0800

    mwehehe

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -84,7 +84,7 @@ const Experience = ({ isDarkMode }: ExperienceProps) => {
 
       <div className="text-center mt-6">
         <a
-          href="/delgado_resume.pdf"
+          href="/resume.pdf"
           target="_blank"
           className={`px-4 py-2 inline-block rounded-lg border ${isDarkMode ? 'border-[#30363d] hover:border-[#58a6ff] text-[#58a6ff]' : 'border-gray-300 hover:border-blue-400 text-blue-600'} transition-colors`}
         >
